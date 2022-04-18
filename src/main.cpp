8a166858052db0a7dac994090a097ee4fc11fd20
Mikaël Capelle <capelle.mikael@gmail.com>
2022-04-18 22:58:36 +0200

    Remove game_features.

--- src/main.cpp
+++ src/main.cpp
@@ -42,12 +42,10 @@ void add_tasks()
 
 	add_task<parallel_tasks>()
 		.add_task<tasks::python>()
+		.add_task<boost>()
+		.add_task<boost_di>()
 		.add_task<lz4>()
 		.add_task<spdlog>();
-		
-    add_task<parallel_tasks>()
-        .add_task<boost>()
-		.add_task<boost_di>();
 
 	add_task<parallel_tasks>()
 		.add_task<sip>()
@@ -73,7 +71,6 @@ void add_tasks()
 		.add_task<mo>("modorganizer-uibase");
 
 	add_task<parallel_tasks>()
-		.add_task<mo>({"modorganizer-game_features", "gamefeatures"})
 		.add_task<mo>("modorganizer-archive")
 		.add_task<mo>("modorganizer-lootcli")
 		.add_task<mo>("modorganizer-esptk")
@@ -105,7 +102,7 @@ void add_tasks()
 
 	add_task<parallel_tasks>()
 		.add_task<mo>({"modorganizer-tool_inieditor", "inieditor"})
-		.add_task<mo>({"modorganizer-tool_inibakery", "inibakery"})
+		.add_task<mo>("modorganizer-tool_inibakery")
 		.add_task<mo>("modorganizer-preview_base")
 		.add_task<mo>("modorganizer-diagnose_basic")
 		.add_task<mo>("modorganizer-check_fnis")
